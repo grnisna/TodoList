9da4e3f91b58e10f24d5a78ee47208aa1401f976
grnisna <mdnisanahmed63@gmail.com>
2023-06-07 23:47:15 +0600

    task component addded

--- src/Components/TaskList/SingleTask.tsx
+++ src/Components/TaskList/SingleTask.tsx
@@ -1,20 +1,32 @@
-import React, { FC } from 'react';
-import Todo from '../Model/Model';
-
+import React, { FC } from "react";
+import Todo from "../Model/Model";
+import { AiFillEdit ,AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import "../Styles/Style.css";
 
 interface Props {
-    task:Todo;
-    allTask:Todo[];
-    setAllTask:React.Dispatch<React.SetStateAction<Todo[]>>
+  task: Todo;
+  allTask: Todo[];
+  setAllTask: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-
-const SingleTask:FC<Props> = ({task, allTask, setAllTask}) => {
-    return (
-        <div>
-            <p>{task.todo}</p>
-        </div>
-    );
+const SingleTask: FC<Props> = ({ task, allTask, setAllTask }) => {
+  return (
+    <form className="single_task">
+      <span className="single_task_text">{task.todo}</span>
+      <div className="all_icons">
+        <span className="icons">
+          <AiFillEdit />
+        </span>
+        <span className="icons">
+          <AiFillDelete />
+        </span>
+        <span className="icons">
+          <MdDone />
+        </span>
+      </div>
+    </form>
+  );
 };
 
 export default SingleTask;
